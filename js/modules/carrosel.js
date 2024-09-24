43715150ac721f292dca2333ff620fa28c4e8b44
Fabiano Bastos <fabiano__bastos@hotmail.com>
2024-09-24 12:33:01 -0300

    INCLUSÃO DOS BANNERS
    Funcionalidade dos banners inclusa

--- js/modules/carrosel.js
+++ js/modules/carrosel.js
@@ -1,57 +1,43 @@
 const img_banner = document.querySelector('#imagem-banner');
 const container_banner = document.querySelector('.banner');
-const previus_carrosel = document.querySelector('#previous');
-const next_carrosel = document.querySelector('#next');
-const video = document.getElementById('video');
+const previous_carousel = document.querySelector('#previous');
+const next_carousel = document.querySelector('#next');
 
-let indice = 1;
-let primeiroSlide;
-function sumirVideo(indi) {
-  if (indi > 1) {
-    video.style.display = 'none';
-  } else {
-    video.style.display = 'block';
-  }
+let index = 1;
+let firstSlide;
+
+function changeImage() {
+    img_banner.setAttribute('src', `imgs/BANNER${index}.png`);
+    nextSlide();
 }
 
-function mudarImagem() {
-  sumirVideo(indice);
-
-  img_banner.setAttribute('src', `./imgs/img${indice}.png`);
-
-  proximoSlide();
+function nextSlide() {
+    if (index < 3) {
+        index++;
+    } else {
+        index = 1;
+    }
+    img_banner.setAttribute('src', `imgs/BANNER${index}.png`);
 }
 
-function proximoSlide() {
-  if (indice < 5) {
-    indice++;
-  } else {
-    indice = 1;
-  }
-
-  sumirVideo(indice);
-  img_banner.setAttribute('src', `./imgs/img${indice}.png`);
+function previousSlide() {
+    if (index > 1) {
+        index--;
+    } else {
+        index = 3;
+    }
+    img_banner.setAttribute('src', `imgs/BANNER${index}.png`);
 }
 
-function slideAnterior() {
-  if (indice > 1) {
-    indice--;
-  } else {
-    indice = 5;
-  }
-  sumirVideo(indice);
-  img_banner.setAttribute('src', `./imgs/img${indice}.png`);
-}
-
-let banner = setInterval(mudarImagem, 5000);
-container_banner.addEventListener('mouseover', (event) => {
-  clearInterval(banner);
+let banner = setInterval(changeImage, 5000);
+container_banner.addEventListener('mouseover', () => {
+    clearInterval(banner);
 });
 
-container_banner.addEventListener('mouseout', (event) => {
-  clearInterval(banner);
-  banner = setInterval(mudarImagem, 5000);
+container_banner.addEventListener('mouseout', () => {
+    clearInterval(banner);
+    banner = setInterval(changeImage, 5000);
 });
 
-next_carrosel.addEventListener('click', proximoSlide);
-previus_carrosel.addEventListener('click', slideAnterior);
+next_carousel.addEventListener('click', nextSlide);
+previous_carousel.addEventListener('click', previousSlide);
